627e51fcf191b83b6f385f4fc43404e0d0fd93ed
emmaso-jpg <emma.sodini@mail.polimi.it>
2026-04-27 18:24:04 +0200

    custom fonts

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,15 @@
 
 <svelte:head>
 	<link rel="icon" href={favicon} />
+
+	<!--Futura font!-->
+	<link rel="stylesheet" href="https://use.typekit.net" />
+	<link rel="stylesheet" href="https://use.typekit.net/gxk4urz.css">
+
+	<!--EB Garamond font-->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&display=swap" rel="stylesheet">
 </svelte:head>
 
 {@render children()}
