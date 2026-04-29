e734ae4483dbd0f081c497fd9cdc82886f451c96
emmaso-jpg <emma.sodini@mail.polimi.it>
2026-04-29 12:39:07 +0200

    remove vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex } from 'mdsvex';
-import adapter from '@sveltejs/adapter-vercel';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
